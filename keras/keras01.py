587592ab2dc0c287e984e5c9f819e9927e21b0a3
young jun <95626180+awJun@users.noreply.github.com>
2022-07-31 22:17:07 +0900

    Update keras01.py

--- keras/keras01.py
+++ keras/keras01.py
@@ -2,7 +2,7 @@
 import numpy as np
 from sklearn.metrics import log_loss
 
-x = np.array([1, 2, 3])  # 배열 리스트 1, 2, 3   # 리스트 한 덩어리가 노드에 들어간다.  
+x = np.array([1, 2, 3])  # 배열 리스트 1, 2, 3   # 리스트 한 덩어리가 노드에 들어간다.   
 y = np.array([1, 2, 3]) 
        
       
